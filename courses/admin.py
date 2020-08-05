2edc83981a591f158b23cec26fc9949fc7a5733b
Tiago Abreu <4563abreu@gmail.com>
2020-08-05 17:36:48 -0300

    app course e paginas de listagem de curso

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Course
 # Register your models here.
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'start_date', 'create_at']
+    search_fields = ['name', 'slug']
+admin.site.register(Course, CourseAdmin)
